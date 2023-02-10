cf820d8467d34d693e4bbda70faf3f1eed9b39d3
Adwaita Raj Modak <adwaitaraj@gmail.com>
2023-02-11 02:06:21 +0530

    Added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app = express();
 const schema = require('./schema/schema.js')
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require("mongoose");
-
+const cors = require('cors');
 mongoose.set('strictQuery', true);
 
+
+const app = express();
+app.use(cors())
 mongoose.connect("mongodb+srv://admin:admin@cluster0.gv2hp4y.mongodb.net/?retryWrites=true&w=majority");
 mongoose.connection.once('open', () => {
     console.log("Connected to MongoDB");
